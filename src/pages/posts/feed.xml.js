e81ad05fb7e774ba676d075a22632bbc97961143
meizuflux <hello@meizuflux.com>
2023-03-29 17:30:15 +0000

    Delete custom RSS stylesheet

--- src/pages/posts/feed.xml.js
+++ src/pages/posts/feed.xml.js
@@ -7,7 +7,6 @@ export const get = () => rss({
     title: "meizuflux's blog",
     description: 'me writing whatever i want pretty much',
     site: "https://meizuflux.com",
-    stylesheet: "/pretty-feed-v3.xsl",
     items: posts.map((post) => ({
       link: post.url,
       site: "test",
