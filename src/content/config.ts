04327e1b2954d4e0def8533a0afd74df025014ef
ppotatoo <hello@meizuflux.com>
2024-01-11 13:29:53 +0000

    lint with biome

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,15 +2,15 @@
 import { z, defineCollection } from "astro:content";
 // Define a `type` and `schema` for each collection
 const postsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      created: z.date(),
-      modified: z.date(),
-      description: z.string()
-    })
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		created: z.date(),
+		modified: z.date(),
+		description: z.string(),
+	}),
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = {
-  posts: postsCollection,
+	posts: postsCollection,
 };
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,24 +1,21 @@
-
-  
 export const getReadingTime = (text: String): Number => {
-    const wpm = 500;
-    const words = text.trim().split(/\a-zA-Z0-9+/).length;
-    return Math.ceil(words / wpm);
-}
+	const wpm = 500;
+	const words = text.trim().split(/\a-zA-Z0-9+/).length;
+	return Math.ceil(words / wpm);
+};
 
 export const getSlug = (file: String): String => {
-    return file.split("/").pop().slice(0, -3)
-}
+	return file.split("/").pop().slice(0, -3);
+};
 
 export const getSimpleDate = (date: Date): String => {
-	return `${date.toLocaleString('default', { month: 'long' })} ${date.getDay()}, ${date.getFullYear()}`
-}
+	return `${date.toLocaleString("default", { month: "long" })} ${date.getDay()}, ${date.getFullYear()}`;
+};
 
 export const sortByYear = (arr): { [key: number]: object } => {
 	return arr.reduce(function (obj, item) {
-
 		// Check if the criteria is a function to run on the item or a property of it
-		var key = item.data.created
+		var key = item.data.created;
 
 		// If the key doesn't exist yet, create it
 		if (!obj.hasOwnProperty(key)) {
@@ -30,6 +27,5 @@ export const sortByYear = (arr): { [key: number]: object } => {
 
 		// Return the object to the next item in the loop
 		return obj;
-
 	}, {});
 };
