06978fb593c8c2e391bd61a28fbdd2ac90f81604
meizuflux <hello@meizuflux.com>
2025-05-30 19:17:14 -0400

    Use experimental Astro font system

--- astro.config.ts
+++ astro.config.ts
@@ -1,28 +1,33 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://meizuflux.com",
-  output: "static",
-  prefetch: true,
-  
-  markdown: {
-    shikiConfig: {
-        theme: "catppuccin-frappe", // see: https://shiki.style/themes
-        /*langs: [
-            'js',
-            "python",
-            "shellscript",
-        ]*/
-    }
-  },
-  build: {
-    format: "preserve",
-    inlineStylesheets: "never"
-  },
-  experimental: {
-    clientPrerender: true,
-  },
-  integrations: [sitemap()]
+    site: "https://meizuflux.com",
+    output: "static",
+    prefetch: true,
+    
+    markdown: {
+        shikiConfig: {
+            theme: "catppuccin-frappe", // see: https://shiki.style/themes
+            /*langs: [
+                'js',
+                "python",
+                "shellscript",
+            ]*/
+        }
+    },
+    build: {
+        format: "preserve",
+        inlineStylesheets: "never"
+    },
+    experimental: {
+        clientPrerender: true,
+        fonts: [{
+            provider: fontProviders.google(),
+            name: "Chivo",
+            cssVariable: "--font-chivo",
+        }]
+    },
+    integrations: [sitemap()]
 });
