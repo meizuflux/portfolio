4c02be46983037f809951cd7888176710d1f7c8d
meizuflux <hello@meizuflux.com>
2025-05-30 19:25:04 -0400

    astro optimizations

--- astro.config.ts
+++ astro.config.ts
@@ -5,21 +5,12 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
     site: "https://meizuflux.com",
     output: "static",
-    prefetch: true,
-    
-    markdown: {
-        shikiConfig: {
-            theme: "catppuccin-frappe", // see: https://shiki.style/themes
-            /*langs: [
-                'js',
-                "python",
-                "shellscript",
-            ]*/
-        }
+    prefetch: {
+        prefetchAll: true,
+        defaultStrategy: "viewport"
     },
     build: {
         format: "preserve",
-        inlineStylesheets: "never"
     },
     experimental: {
         clientPrerender: true,
